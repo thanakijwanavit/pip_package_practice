8e40e8f058e2dfc7c03f340c2f63aa41aafb674b
nic <nwanavit@hatari.cc>
2019-06-15 14:47:26 +0000

    initial commit

---
+++ 3a_python_package/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(name='gaussian distribution',
+      version='1',
+      description='gaussian distribution',
+      zip_safe=False
+     )
---
+++ 3a_python_package/test.py
@@ -0,0 +1,5 @@
+from distribution import Gaussian
+
+dist=Gaussian(4,5)
+print(dist.calculate_mean())
+
